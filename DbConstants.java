e45ba64fb6167f29e97b674497f1ab2ec100f295
Mingkai880904 <43432006+Mingkai880904@users.noreply.github.com>
2021-03-27 01:52:45 +0800

    Add files via upload

---
+++ DbConstants.java
@@ -0,0 +1,10 @@
+package com.example.sqliteapp;
+
+import  android.provider.BaseColumns;
+
+public interface DbConstants extends BaseColumns{
+    public static final String TABLE_NAME="friends";
+    public static final String NAME="name";
+    public static final String TEL="tel";
+    public static final String EMAIL="email";
+}
